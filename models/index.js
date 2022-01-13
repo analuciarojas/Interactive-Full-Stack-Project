3a06c847b415856035869a3129f4f888ccddf36a
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-01-12 20:16:38 -0800

    model basic index

--- models/index.js
+++ models/index.js
@@ -1,34 +1,5 @@
 const User = require("./User");
-const Categories = require("./Categories)");
-const Quiz = require("quiz");
+const Category = require("./Category)");
+const Quiz = require("./Quiz");
 
-// create associations
-User.hasMany(Quiz, {
-  foreignKey: "user_id",
-});
-
-Quiz.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Quiz.belongsToMany(User, {
-  foreignKey: "quiz_id",
-});
-
-Category.belongsTo(Quiz, {
-  foreignKey: "quiz_id",
-});
-
-Quiz.belongsTo(uiz, {
-  foreignKey: "post_id",
-});
-
-User.hasMany(Comment, {
-  foreignKey: "user_id",
-});
-
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
-});
-
-module.exports = { User, Post, Comment };
+module.exports = { User, Category, Quiz };
