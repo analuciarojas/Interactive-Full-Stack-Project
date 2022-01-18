3c72abd22a0bb9d2052db6b4b5676b0693258c95
Seeky <seek916@gmail.com>
2022-01-17 16:58:48 -0800

    routes setup for Users, quiz, and Category

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,17 @@
-const homeRoutes = require('./home-routes.js');
-
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api')
+
+
+
 router.use('/', homeRoutes);
+
+router.use('/', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening...'));
 });
