c291c44c53d80b96176e0fff91e1ca9fe9883172
Ldeguzman9 <90979638+Ldeguzman9@users.noreply.github.com>
2022-01-18 14:10:46 -0800

    pull

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const path = require('./public/stylesheets/styles.css')
+const express = require("express");
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
+const path = require("./public/stylesheets/styles.css");
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
+// connect the db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening...'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}!`));
 });
