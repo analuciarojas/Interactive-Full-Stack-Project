a524677dea2d83255b3e96ff65b9a1c638cc69ac
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-01-18 19:42:21 -0800

    added materialize css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('main', {
+    res.render('index', {
         id: 1,
         post_url: '.',
         title: 'Quick Quack Quiz',
